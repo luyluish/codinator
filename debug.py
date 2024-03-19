afa9b32a1f8c68aaf152ec0ac71efc4dd166bf26
MicaelTargino <micaelgomestargino@gmail.com>
2024-03-18 21:54:01 -0300

    completed integration and solved flux bugs

--- debug.py
+++ debug.py
@@ -1,13 +1,11 @@
 from random import randint
 
-print("\033[1;34mBEM VINDO AO CODINATOR !!\033[m")
-
 perguntas = {
     1:  {"pergunta": "(Ampla) Você se considera um bom líder em grupos? [s/n] ", "sim": {"eng_software"}, "nao": {"front"}},
     2:  {"pergunta": "(Ampla) Você prefere trabalhar sozinho e não receber ordens? [s/n] ", "sim": {"eng_software", "analise_sis"}, "nao": {"front"}},
     3:  {"pergunta": "(Ampla) Você traz ideias novas constantemente para resolver problemas? [s/n] ", "sim": {"eng_software", "analise_sis"}, "nao": {"front"}},
-    4:  {"pergunta": "(Ampla) Você se considera uma pessoa criativa? [s/n] ", "sim": {"front", "apps"}, "nao": {"dados"}},
-    5:  {"pergunta": "(Ampla) Você se considera uma pessoa organizada? [s/n] ", "sim": {"eng_software", "analise_sis"}, "nao": {"front"}},
+    4:  {"pergunta": "(Ampla) Você se considera uma pessoa criativa? [s/n] ", "sim": {"eng_software", "analise_sis"}, "nao": {"front"}},
+    5:  {"pergunta": "(Ampla)Você se considera uma pessoa organizada? [s/n] ", "sim": {"eng_software", "analise_sis"}, "nao": {"front"}},
     6:  {"pergunta": "Você gosta de construir coisas novas? [s/n] ", "sim": {"eng_software", "apps", "back", "front"}, "nao": {"seguranca"}},
     7:  {"pergunta": "Você é contratado de alguma empresa? [s/n] ", "sim": {"eng_software", "seguranca", "dados", "back"}, "nao": {"front"}},
     8:  {"pergunta": "Você tem interesse em trabalhar na web? [s/n] ", "sim": {"eng_software", "front", "back", "analise_sis"}, "nao": {"dados", "apps", "seguranca"}},
@@ -16,17 +14,17 @@ perguntas = {
     11: {"pergunta": "Você trabalharia com um alto volume de informações? [s/n] ", "sim": {"dados", "seguranca", "back"}, "nao": {"front", "apps"}},
     12: {"pergunta": "Você gostaria de expressar seu lado artístico no seu trabalho? [s/n] ", "sim": {"front", "apps"}, "nao": {"dados", "seguranca"}},
     13: {"pergunta": "Você gostaria de trabalhar criando jogos? [s/n] ", "sim": {"apps"}, "nao": {"dados", "seguranca"}},
-    14: {"pergunta": "Você gosta de lidar diretamente com clientes no desenvolvimento de um projeto? [s/n] ", "sim": {"front", "analise_sis", }, "nao": {"dados", "seguranca"}}
+    14: {"pergunta": "Você gosta de lidar diretamente com clientes no desenvolvimento de um projeto? [s/n] ", "sim": {"front", "analise_sis", }, "nao": {"dados", "seguranca"}},
 }
 
 perguntasFinais = {
     "apps": {"pergunta": "(Especifica) Você gostaria de trabalhar com dispositivos mobile? [s/n] "},
     "front": {"pergunta": "(Específica) Você gosta de design ou desenho? [s/n] "},
-    "back": {"pergunta": "(Especifica) Back? [s/n] "},
+    "back": {"pergunta": "(Especifica) Você gosta de design ou desenho? [s/n] "},
     "analise_sis": {"pergunta": "(Especifica) Análise? [s/n] "},
     "seguranca": {"pergunta": "(Especifica) Dados? [s/n] "},
     "dados": {"pergunta": "(Especifica) Segurança? [s/n] "},
-    "eng_software": {"pergunta": "(Especifica) Engenharia de Software? [s/n] "}
+    "eng_software": {"pergunta": "(Especifica) Engenharia de Software? [s/n] "},
 }
 
 scores = {
@@ -36,20 +34,24 @@ scores = {
     "analise_sis": 0,
     "seguranca": 0,
     "dados": 0,
-    "eng_software": 0
+    "eng_software": 0,
 }
 
 lista_perguntas = []
 
 def perguntarAmplas(x = 5):
+    print("Iniciando perguntas amplas.")
     while(len(lista_perguntas) < x):
+        print(f"Scores atual: {scores}")
         for area in scores:
             if scores[area] == 5:
                 perguntarFinal(area.index)
         a = randint(1, x)
         if a not in lista_perguntas:
             print("/--------------/")
+            print(f"Realizando pergunta {a}")
             b = str(input(perguntas[a]["pergunta"]))
+            print(perguntas[a]["pergunta"] + b)
             if b[0] == "s" or b[0] == "S":
                 for score in perguntas[a]["sim"]:
                     scores[score] += 1
@@ -62,26 +64,22 @@ def perguntarAmplas(x = 5):
             lista_perguntas.append(a)
 
 def checar_vencedor():
+    print("Checando vencedor...")
     for area in scores:
         if scores[area] == 5:
+            print(f"Vencedor encontrado: {area}")
             return area
+    print("Nenhum vencedor ainda.")
     return -1
 
-def checar_maior():
-    maior = -999
-    indice_maior = -999
-    for area in scores:
-        if scores[area] > maior:
-            maior = scores[area]
-            indice_maior = area
-    return indice_maior
-
 def perguntar_especifico(y = 14):
-    print("/--------------/")
+    print("Iniciando perguntas específicas.")
     while checar_vencedor() == -1:
         c = randint(6, y)
         if c not in lista_perguntas:
+            print(f"Realizando pergunta específica {c}")
             d = str(input(perguntas[c]["pergunta"]))
+            print(perguntas[c]["pergunta"] + d)
             lista_perguntas.append(c)
             if d[0] == 's' or d[0] == 'S':
                 for score in perguntas[c]["sim"]:
@@ -89,20 +87,24 @@ def perguntar_especifico(y = 14):
             elif d[0] == 'n' or d[0] == 'N':
                 for score in perguntas[c]["nao"]:
                     scores[score] += 1
-        if len(lista_perguntas) >= 14:
-            break
-    if len(lista_perguntas) < 14:
-        perguntarFinal(checar_vencedor())
-    else:
-        encerrarPrograma(checar_maior())
+    print(f"Scores após perguntas específicas: {scores}")
+    perguntarFinal(checar_vencedor())
 
 def perguntarFinal(area):
+    print(f"Iniciando pergunta final para área {area}.")
     resposta = str(input(perguntasFinais[area]["pergunta"]))
-    if resposta[0] == "s" or resposta[0] == "S":
-        encerrarPrograma(area)
-    elif resposta[0] == "n" or resposta[0] == "N":
-        scores[area] += 1
-        perguntar_especifico()
+    print(perguntasFinais[area]["pergunta"] + resposta)
+    if area == "front" or area == "back":
+        if resposta[0] == "s" or resposta[0] == "S":
+            encerrarPrograma("front")
+        elif resposta[0] == "n" or resposta[0] == "N":
+            encerrarPrograma("back")
+    else:
+        if resposta[0] == "s" or resposta[0] == "S":
+            encerrarPrograma(area)
+        elif resposta[0] == "n" or resposta[0] == "N":
+            scores[area] += 1
+            perguntar_especifico()
 
 def encerrarPrograma(vencedor):
     print("Resultado: ")
@@ -124,6 +126,7 @@ def encerrarPrograma(vencedor):
 def main():
     print("Iniciando o programa.")
     perguntarAmplas()
+    print(f"Scores após perguntas amplas: {scores}")
     perguntar_especifico()
     print(f"Scores finais: {scores}")
 
